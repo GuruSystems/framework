e5ddb912a2dfd405e93ec140f7ebcff61c21556f
Conrad Wood <cnw@singingcat.net>
2018-04-21 20:26:19 +0100

    * second critical bugfix:   Current behaviour is for the roundrobin strategy to   get targets on start. Thereafter it round-robins between   the known targets. It does not refresh the list of   targets ever.   In a scenario where a service is redeployed all targets,   as known initially to the dialer, are removed and replaced   with new targets.   Unless the dialer queries the registry to find out about   this change it will loose connection forever.   this patch intends to change that.   Developers of modules should update to this version   as soon as practically possible.

--- src/golang.gurusys.co.uk/go-framework/client/resolver.go
+++ src/golang.gurusys.co.uk/go-framework/client/resolver.go
@@ -1,71 +1,30 @@
 package client
 
 import (
-	"fmt"
-	pb "golang.gurusys.co.uk/apis/registry"
-	"golang.gurusys.co.uk/go-framework/cmdline"
-	"golang.gurusys.co.uk/go-framework/registry"
 	"google.golang.org/grpc/naming"
-	"math/rand"
-	"time"
-)
-
-var (
-	randomer = rand.New(rand.NewSource(time.Now().Unix()))
 )
 
 type RegistryResolver struct{}
 
 func (r *RegistryResolver) Resolve(serviceName string) (naming.Watcher, error) {
-	if *dialer_debug {
-		fmt.Printf("Resolving service address \"%s\" via registry %s...\n", serviceName, cmdline.GetRegistryAddress())
-	}
-	var err error
-	var serverAddresses []string
-	for {
-		// if we find 0 addresses (or have an error), e.g. registry is unavailable
-		// we got to sleep and try again every 5 seconds(ish)
-		serverAddresses, err = registry.GetHosts(serviceName, pb.Apitype_grpc)
-		if err == nil {
-			break
-		}
-		time.Sleep(time.Second * time.Duration(randomer.Intn(10)))
-
-		fmt.Printf("error retrieving hosts for %s: %s\n", serviceName, err)
-
-	}
-	if *dialer_debug {
-		fmt.Printf("Instances of %s: %s\n", serviceName, serverAddresses)
-	}
-	// we randomize the server addresses we got initially.
-	// otherwise we always end up hitting the same thing
-
-	for i := len(serverAddresses) - 1; i > 0; i-- {
-		j := randomer.Intn(i + 1)
-		serverAddresses[i], serverAddresses[j] = serverAddresses[j], serverAddresses[i]
-	}
-	var ups []*naming.Update
-	for _, a := range serverAddresses {
-		ups = append(
-			ups,
-			&naming.Update{naming.Add, a, ""},
-		)
-	}
+	//panic("wtf")
 
 	var ch chan []*naming.Update = make(chan []*naming.Update, 1)
-	ch <- ups
-
-	return &staticWatcher{ch}, nil
+	sw := &targetWatcher{updates: ch, serviceName: serviceName}
+	notifyTargetChanges(sw)
+	return sw, nil
 }
 
-type staticWatcher struct {
-	updates chan []*naming.Update
+type targetWatcher struct {
+	updates        chan []*naming.Update
+	currentTargets []string
+	serviceName    string
 }
 
-func (w *staticWatcher) Next() ([]*naming.Update, error) {
+func (w *targetWatcher) Next() ([]*naming.Update, error) {
 	return <-w.updates, nil
 }
 
-func (w *staticWatcher) Close() {
+func (w *targetWatcher) Close() {
 	close(w.updates)
 }
---
+++ src/golang.gurusys.co.uk/go-framework/client/targets.go
@@ -0,0 +1,163 @@
+package client
+
+import (
+	"flag"
+	"fmt"
+	pb "golang.gurusys.co.uk/apis/registry"
+	"golang.gurusys.co.uk/go-framework/cmdline"
+	"golang.gurusys.co.uk/go-framework/registry"
+	"google.golang.org/grpc/naming"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// we keep a local "mirror" copy of the registry
+// so that, in case the registry goes away for some time
+// we can still do our load balancing.
+// we also do that because we need to regularly requery the registry
+// to find updates to our target locations.
+
+// to enable periodic updates, call notifyTargetChanges(servicename/channel)
+// and this thing will inform the channel of changes
+
+// notifyTargetChanges() will block until at least one target becomes
+// available.
+// This should prevent services from coming up and immediately failing
+// because they're missing a backend.
+// this might cause trouble during testing, if so a variable is provided to
+// change that behavior
+
+var (
+	randomer          = rand.New(rand.NewSource(time.Now().Unix()))
+	dialer_wait_first = flag.Bool("dial_wait_for_target", true, "initially block until at least one target becomes available")
+	targetNotifiers   []*targetNotifier
+	notifierLock      sync.Mutex
+)
+
+type targetNotifier struct {
+	serviceName string
+	lock        sync.Mutex // lock updates to watchers
+	watchers    []*targetWatcher
+}
+
+func GetOrCreateNotifier(service string) *targetNotifier {
+	notifierLock.Lock()
+	defer notifierLock.Unlock()
+	for _, tn := range targetNotifiers {
+		if tn.serviceName == service {
+			return tn
+		}
+	}
+	tn := &targetNotifier{serviceName: service}
+	targetNotifiers = append(targetNotifiers, tn)
+	go tn.requery()
+	return tn
+}
+
+func notifyTargetChanges(ch *targetWatcher) error {
+	if *dialer_debug {
+		fmt.Printf("New notifier requested for %s\n", ch.serviceName)
+	}
+	tn := GetOrCreateNotifier(ch.serviceName)
+	tn.AddWatcher(ch)
+	return nil
+}
+
+// get a list of instances
+func queryForActiveInstances(serviceName string) []string {
+
+	if *dialer_debug {
+		fmt.Printf("Resolving service address \"%s\" via registry %s...\n", serviceName, cmdline.GetRegistryAddress())
+	}
+	var err error
+	var serverAddresses []string
+	serverAddresses, err = registry.GetHosts(serviceName, pb.Apitype_grpc)
+	// error. so what?
+	if err != nil {
+		if *dialer_debug {
+			fmt.Printf("error retrieving hosts for %s: %s\n", serviceName, err)
+		}
+	} else {
+		if *dialer_debug {
+			fmt.Printf("Instances of %s: %s\n", serviceName, serverAddresses)
+		}
+	}
+
+	return serverAddresses
+
+}
+
+// this thing runs in the background, one per servicename
+func (tn *targetNotifier) requery() {
+	for {
+		time.Sleep(time.Duration(randomer.Intn(30)) * time.Second)
+		serverAddresses := queryForActiveInstances(tn.serviceName)
+		var ups []*naming.Update
+		for _, a := range serverAddresses {
+			ups = append(
+				ups,
+				&naming.Update{naming.Add, a, ""},
+			)
+		}
+		tn.lock.Lock()
+		curWatchers := make([]*targetWatcher, len(tn.watchers))
+		i := 0
+		for _, w := range tn.watchers {
+			curWatchers[i] = w
+			i++
+		}
+		tn.lock.Unlock()
+		for _, watcher := range curWatchers {
+			sendDiff(serverAddresses, watcher)
+			//			watcher.updates <- ups
+		}
+	}
+}
+
+// work out changes (compared to this particular watcher)
+func sendDiff(serviceAddresses []string, tw *targetWatcher) {
+	// take care of NEW services
+	var ups []*naming.Update
+	for _, sa := range serviceAddresses {
+		if !isInArray(tw.currentTargets, sa) {
+			ups = append(ups, &naming.Update{naming.Add, sa, ""})
+			if *dialer_debug {
+				fmt.Printf("notifying watcher of new target %s\n", sa)
+				tw.currentTargets = append(tw.currentTargets, sa)
+			}
+
+		}
+	}
+
+	// take care of REMOVED services
+	for i, sa := range tw.currentTargets {
+		if !isInArray(serviceAddresses, sa) {
+			ups = append(ups, &naming.Update{naming.Delete, sa, ""})
+			if *dialer_debug {
+				fmt.Printf("notifying watcher of REMOVED target %s\n", sa)
+				tw.currentTargets = append(tw.currentTargets[:i], tw.currentTargets[i+1:]...)
+			}
+
+		}
+	}
+
+	// this should be truly async I guess
+	tw.updates <- ups
+
+}
+
+func isInArray(s []string, find string) bool {
+	for _, x := range s {
+		if x == find {
+			return true
+		}
+	}
+	return false
+}
+
+func (tn *targetNotifier) AddWatcher(watcher *targetWatcher) {
+	tn.lock.Lock()
+	tn.watchers = append(tn.watchers, watcher)
+	tn.lock.Unlock()
+}
